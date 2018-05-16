f185c150f119f9944fbb6c3018b671eb2d1c0276
Dr. Alex <alex.gouaillard@cosmosoftware.io>
2018-05-16 16:30:58 -0700

    BUG: clean visibility link warnings on mac.

--- plugins/websocket-client/WebsocketClient.h
+++ plugins/websocket-client/WebsocketClient.h
@@ -2,14 +2,17 @@
 #define _WEBSOCKETCLIENT_H_
 
 #ifdef _MSC_VER
-
-#ifdef WEBSOCKETCLIENT_EXPORTS
-#define WEBSOCKETCLIENT_API __declspec(dllexport)
+#  ifdef WEBSOCKETCLIENT_EXPORTS
+#    define WEBSOCKETCLIENT_API __declspec(dllexport)
+#  else
+#    define WEBSOCKETCLIENT_API __declspec(dllimport)
+#  endif
 #else
-#define WEBSOCKETCLIENT_API __declspec(dllimport)
-#endif
-#else
-#define WEBSOCKETCLIENT_API 
+#  ifdef __APPLE__
+#    define WEBSOCKETCLIENT_API __attribute__((visibility("default")))
+#  else
+#    define WEBSOCKETCLIENT_API
+#  endif
 #endif
 
 #include <string>
@@ -20,21 +23,21 @@
 class WEBSOCKETCLIENT_API WebsocketClient
 {
 public:
-	class Listener
-	{
-	public:
-		virtual void onConnected() = 0;
-		virtual void onLogged(int code) = 0;
-		virtual void onLoggedError(int code) = 0;
-		virtual void onOpened(const std::string &sdp) = 0;
-		virtual void onOpenedError(int code) = 0;
-		virtual void onDisconnected() = 0;
-	};
+  class Listener
+  {
+  public:
+    virtual void onConnected() = 0;
+    virtual void onLogged(int code) = 0;
+    virtual void onLoggedError(int code) = 0;
+    virtual void onOpened(const std::string &sdp) = 0;
+    virtual void onOpenedError(int code) = 0;
+    virtual void onDisconnected() = 0;
+  };
 public:
-	virtual bool connect(std::string url, long long room, std::string username, std::string token, Listener* listener) = 0;
-	virtual bool open(const std::string &sdp) = 0;
-	virtual bool trickle(const std::string &mid, int index, const std::string &candidate, bool last) = 0;
-	virtual bool disconnect(bool wait) = 0;
+  virtual bool connect(std::string url, long long room, std::string username, std::string token, Listener* listener) = 0;
+  virtual bool open(const std::string &sdp) = 0;
+  virtual bool trickle(const std::string &mid, int index, const std::string &candidate, bool last) = 0;
+  virtual bool disconnect(bool wait) = 0;
 
 };
 
