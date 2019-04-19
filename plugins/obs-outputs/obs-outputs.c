c651fe5985ba9edec0363bff4ed20b9fbc805360
alex gouaillard <agouaillard@gmail.com>
2019-04-19 23:23:56 +0700

    WIP: Removed dependency to webrtc-openssl.
    - used libwebrtc 73 (sframe)
    - Removed dependency to webrtc when it comes to openssl
    - Using openssl 1.1.0 installed with brew on mac
    - add to temporaly comment out rtmp
    - add to temporaly comment out FTL
    - compiles, not tested.

--- plugins/obs-outputs/obs-outputs.c
+++ plugins/obs-outputs/obs-outputs.c
@@ -4,8 +4,8 @@
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-outputs", "en-US")
 
-extern struct obs_output_info flv_output_info;
-extern struct obs_output_info rtmp_output_info;
+// extern struct obs_output_info flv_output_info;
+// extern struct obs_output_info rtmp_output_info;
 extern struct obs_output_info null_output_info;
 extern struct obs_output_info janus_output_info;
 extern struct obs_output_info spankchain_output_info;
@@ -13,8 +13,8 @@ extern struct obs_output_info millicast_output_info;
 
 bool obs_module_load(void)
 {
-  obs_register_output(&flv_output_info);
-  obs_register_output(&rtmp_output_info);
+  // obs_register_output(&flv_output_info);
+  // obs_register_output(&rtmp_output_info);
   obs_register_output(&null_output_info);
   obs_register_output(&janus_output_info);
   obs_register_output(&spankchain_output_info);
