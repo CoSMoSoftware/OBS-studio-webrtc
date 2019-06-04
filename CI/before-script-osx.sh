b7384927b898cfc8012e6e5d5fad5e04d51c5f51
Dr Alexandre Gouaillard <alex.gouaillard@cosmosoftware.io>
2019-06-04 16:21:01 +0800

    WIP: point CMake to openssl 1.1

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,11 +3,12 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 mkdir build
 cd build
 cmake \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
--DCMAKE_BUILD_TYPE=RELEASE \
 -DDepsPath=/tmp/obsdeps \
+-DCMAKE_BUILD_TYPE=RELEASE \
 -DVLCPath=$PWD/../../vlc-master \
--Dlibwebrtc_DIR=/tmp/libWebRTC-73.0-x64-Rel-COMMUNITY-BETA/cmake \
 -DQTDIR=/usr/local/Cellar/qt/5.10.1 \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
+-Dlibwebrtc_DIR=/tmp/libWebRTC-73.0-x64-Rel-COMMUNITY-BETA/cmake \
 ..
 cmake --build .
