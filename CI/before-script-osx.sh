269d1db53658ec30c6f3e018fd18a05996fa949a
Dr Alexandre Gouaillard <alex.gouaillard@cosmosoftware.io>
2019-06-04 14:49:40 +0800

    WIP: add support for QT 5.10 the OBS way.

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -2,9 +2,12 @@
 export PATH=/usr/local/opt/ccache/libexec:$PATH
 mkdir build
 cd build
-cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+cmake \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+-DCMAKE_BUILD_TYPE=RELEASE \
 -DDepsPath=/tmp/obsdeps \
 -DVLCPath=$PWD/../../vlc-master \
 -Dlibwebrtc_DIR=/tmp/libWebRTC-73.0-x64-Rel-COMMUNITY-BETA \
--DCMAKE_BUILD_TYPE=RELEASE ..
+-DQTDIR=/usr/local/Cellar/qt/5.10.1 \
+..
 cmake --build .
