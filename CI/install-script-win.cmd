73de634870d3d74d4ad099e170a87f586d18a224
Dr Alex Gouaillard <agouaillard@gmail.com>
2019-06-22 00:45:02 +0800

    WIP: windows build.

--- CI/install-script-win.cmd
+++ CI/install-script-win.cmd
@@ -37,4 +37,3 @@ set build_config=Release
 mkdir build64
 cd build64
 cmake -G "Visual Studio 15 2017 Win64" -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DBUILD_CAPTIONS=true -DCOMPILE_D3D12_HOOK=true -DBUILD_BROWSER=true -DCEF_ROOT_DIR=%CEF_64% -Dlibwebrtc_DIR=%libwebrtcPath% -DOPENSSL_ROOT_DIR=%opensslPath%  ..
-cd ..
