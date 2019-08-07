b6ad5db3ea8bd3a6e7465a735987422dd55f7876
Dr. Alex <alex.gouaillard@cosmosoftware.io>
2019-08-08 00:30:28 +0800

    ENH: skinned builds by default.

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -4,11 +4,13 @@ mkdir build
 cd build
 cmake \
 -DDepsPath=/tmp/obsdeps \
--DCMAKE_BUILD_TYPE=RELEASE \
+-DCMAKE_BUILD_TYPE=Release \
 -DVLCPath=$PWD/../../vlc-master \
 -DQTDIR=/usr/local/Cellar/qt/5.10.1 \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
 -Dlibwebrtc_DIR=/tmp/libWebRTC-73.0-x64-Rel-COMMUNITY-BETA/cmake \
+-DBUILD_BROWSER=false \
+-DOBS_WEBRTC_VENDOR_NAME=Evercast \
 ..
 
