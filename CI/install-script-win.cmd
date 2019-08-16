78d350ff39ff1d2cb0c0b7f2ded8e9b75c3ffca9
Valentin Ramirez <valentin.ramirez@cosmosoftware.io>
2019-08-16 11:38:47 +0800

    ENH: Attempt to retrieve libwebrtc windows community build from S3

--- CI/install-script-win.cmd
+++ CI/install-script-win.cmd
@@ -15,11 +15,8 @@ rem if exist cef_binary_%CEF_VERSION%_windows64.zip (curl -kLO https://cdn-fastl
 rem 7z x cef_binary_%CEF_VERSION%_windows64.zip -oCEF_64
 rem set CEF_64=%CD%\CEF_64\cef_binary_%CEF_VERSION%_windows64
 rem ---------------------------------------------------------------------
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1mgOr53httBCxmmIoln4VozIdrw-sOUuY" > nul
-for /f "delims="  %%x in ('findstr /C:"NID" cookie') do set "confirm_line=%%x"
-for /f "tokens=2 delims==" %%a in ("%confirm_line%") do set "confirm_id=%%a"
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=%confirm_id%&id=1mgOr53httBCxmmIoln4VozIdrw-sOUuY" -olibWebRTC-73.0-x64-Rel-msvc2017-COMMUNITY-BETA.zip
-7z x libWebRTC-73.0-x64-Rel-msvc2017-COMMUNITY-BETA.zip -olibWebRTC-73
+curl -Lb ./cookie "https://libwebrtc-community-builds.s3.amazonaws.com/libWebRTC-73-win.tar.gz" -olibWebRTC-73-win.tar.gz
+tar -xzf libWebRTC-73-win.tar.gz
 set libwebrtcPath=%CD%\libWebRTC-73\cmake
 rem ---------------------------------------------------------------------
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1nwuNAq2N9egnVGCmZ-_3JlUCI6-EroSL" > nul
