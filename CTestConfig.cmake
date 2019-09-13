40d19c0bd56a98aa336604450395b426ff87cd12
Dr. Alex Gouaillard <alex.gouaillard@cosmosoftware.io>
2019-09-13 10:52:54 +0200

    BUG: Revert of previous commit as public cdash uses http.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,7 +2,7 @@
 set(CTEST_PROJECT_NAME "OBS-Studio-WebRTC")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=OBS-Studio-WebRTC")
 set(CTEST_DROP_SITE_CDASH TRUE)
