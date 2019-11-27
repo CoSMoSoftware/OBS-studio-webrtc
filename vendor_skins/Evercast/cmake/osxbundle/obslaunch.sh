05c7b24b3f33e40e6cf7acd32caee59fa307a6dd
Guilherme Castro <skastr052@gmail.com>
2019-11-27 16:58:51 -0300

    Add workaround for Catalina

--- vendor_skins/Evercast/cmake/osxbundle/obslaunch.sh
+++ vendor_skins/Evercast/cmake/osxbundle/obslaunch.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-cd ../Resources/bin
-exec ./ebs "$@"
+# use argument 1 as the version or get it from sw_vers
+os_ver=$(sw_vers -productVersion)
 
+if [[ "$os_ver" == 10.15.* ]]; then
+	echo "macOS Catalina"
+	osascript -e "tell app \"Terminal\" to do script \"cd /Applications/EBS.app/Contents/Resources/bin && ./ebs\""
+else
+	cd "$(dirname "$0")"
+  cd ../Resources/bin
+  exec ./ebs "$@"
+fi
