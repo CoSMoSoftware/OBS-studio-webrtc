8e8e43034a725537c9cbf293e333360c503b00e7
David Baker <dave@matrix.org>
2020-02-28 17:17:27 +0000

    CI: Fix case of PlugIns directory
    'PlugIns' is normal for MacOS and whatever is generating qt.conf
    (macdeployqt?) is using 'PlugIns', so this both builds and generates
    an app that works on case insensitive filesystems but fails to load
    on case sensitive filesystems.
    
    I haven't tested building the installer, only modifying the
    qt.conf in the installed copy of OBS.app.
    
    Fixes https://github.com/obsproject/obs-studio/issues/2302

--- CI/install/osx/packageApp.sh
+++ CI/install/osx/packageApp.sh
@@ -3,41 +3,40 @@ rm -rf ./EBS.app
 mkdir EBS.app
 mkdir EBS.app/Contents
 mkdir EBS.app/Contents/MacOS
-mkdir EBS.app/Contents/Plugins
+mkdir EBS.app/Contents/PlugIns
 mkdir EBS.app/Contents/Resources
 
 BUILD_CONFIG=RELEASE
-
 cp -r rundir/$BUILD_CONFIG/bin/ ./EBS.app/Contents/MacOS
 cp -r rundir/$BUILD_CONFIG/data ./EBS.app/Contents/Resources
 cp ../CI/install/osx/obs.icns ./EBS.app/Contents/Resources
-cp -r rundir/$BUILD_CONFIG/obs-plugins/ ./EBS.app/Contents/Plugins
+cp -r rundir/$BUILD_CONFIG/obs-plugins/ ./EBS.app/Contents/PlugIns
 cp ../CI/install/osx/Info.plist ./EBS.app/Contents
 
 ../CI/install/osx/dylibBundler -b -cd -d ./EBS.app/Contents/Frameworks -p @executable_path/../Frameworks/ \
 -s ./EBS.app/Contents/MacOS \
 -s /usr/local/opt/mbedtls/lib/ \
--x ./EBS.app/Contents/Plugins/coreaudio-encoder.so \
--x ./EBS.app/Contents/Plugins/decklink-ouput-ui.so \
--x ./EBS.app/Contents/Plugins/frontend-tools.so \
--x ./EBS.app/Contents/Plugins/image-source.so \
--x ./EBS.app/Contents/Plugins/linux-jack.so \
--x ./EBS.app/Contents/Plugins/mac-avcapture.so \
--x ./EBS.app/Contents/Plugins/mac-capture.so \
--x ./EBS.app/Contents/Plugins/mac-decklink.so \
--x ./EBS.app/Contents/Plugins/mac-syphon.so \
--x ./EBS.app/Contents/Plugins/mac-vth264.so \
--x ./EBS.app/Contents/Plugins/obs-ffmpeg.so \
--x ./EBS.app/Contents/Plugins/obs-filters.so \
--x ./EBS.app/Contents/Plugins/obs-transitions.so \
--x ./EBS.app/Contents/Plugins/obs-vst.so \
--x ./EBS.app/Contents/Plugins/rtmp-services.so \
+-x ./EBS.app/Contents/PlugIns/coreaudio-encoder.so \
+-x ./EBS.app/Contents/PlugIns/decklink-ouput-ui.so \
+-x ./EBS.app/Contents/PlugIns/frontend-tools.so \
+-x ./EBS.app/Contents/PlugIns/image-source.so \
+-x ./EBS.app/Contents/PlugIns/linux-jack.so \
+-x ./EBS.app/Contents/PlugIns/mac-avcapture.so \
+-x ./EBS.app/Contents/PlugIns/mac-capture.so \
+-x ./EBS.app/Contents/PlugIns/mac-decklink.so \
+-x ./EBS.app/Contents/PlugIns/mac-syphon.so \
+-x ./EBS.app/Contents/PlugIns/mac-vth264.so \
+-x ./EBS.app/Contents/PlugIns/obs-ffmpeg.so \
+-x ./EBS.app/Contents/PlugIns/obs-filters.so \
+-x ./EBS.app/Contents/PlugIns/obs-transitions.so \
+-x ./EBS.app/Contents/PlugIns/obs-vst.so \
+-x ./EBS.app/Contents/PlugIns/rtmp-services.so \
 -x ./EBS.app/Contents/MacOS/obs \
 -x ./EBS.app/Contents/MacOS/obs-ffmpeg-mux \
--x ./EBS.app/Contents/Plugins/obs-x264.so \
--x ./EBS.app/Contents/Plugins/text-freetype2.so \
--x ./EBS.app/Contents/Plugins/obs-libfdk.so
-# -x ./EBS.app/Contents/Plugins/obs-outputs.so \
+-x ./EBS.app/Contents/PlugIns/obs-x264.so \
+-x ./EBS.app/Contents/PlugIns/text-freetype2.so \
+-x ./EBS.app/Contents/PlugIns/obs-libfdk.so
+# -x ./EBS.app/Contents/PlugIns/obs-outputs.so \
 
 /usr/local/Cellar/qt/5.10.1/bin/macdeployqt ./EBS.app
 
@@ -49,17 +48,17 @@ chmod +w ./EBS.app/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork
 install_name_tool -change /usr/local/Cellar/qt/5.10.1/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork
 
 # decklink ui qt
-install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/decklink-ouput-ui.so
-install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/decklink-ouput-ui.so
-install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/decklink-ouput-ui.so
+install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/PlugIns/decklink-ouput-ui.so
+install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/PlugIns/decklink-ouput-ui.so
+install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/PlugIns/decklink-ouput-ui.so
 
 # frontend tools qt
-install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/frontend-tools.so
-install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/frontend-tools.so
-install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/frontend-tools.so
+install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/PlugIns/frontend-tools.so
+install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/PlugIns/frontend-tools.so
+install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/PlugIns/frontend-tools.so
 
 # vst qt
-install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/obs-vst.so
-install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/obs-vst.so
-install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/obs-vst.so
-install_name_tool -change /usr/local/opt/qt/lib/QtMacExtras.framework/Versions/5/QtMacExtras @executable_path/../Frameworks/QtMacExtras.framework/Versions/5/QtMacExtras ./EBS.app/Contents/Plugins/obs-vst.so
+install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/PlugIns/obs-vst.so
+install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/PlugIns/obs-vst.so
+install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/PlugIns/obs-vst.so
+install_name_tool -change /usr/local/opt/qt/lib/QtMacExtras.framework/Versions/5/QtMacExtras @executable_path/../Frameworks/QtMacExtras.framework/Versions/5/QtMacExtras ./EBS.app/Contents/PlugIns/obs-vst.so
