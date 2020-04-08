4302cc6ff0f6d7dae6349c9f95c6213d384e7650
Steven Westbrook <stevenwestbrook@Stevens-MacBook-Pro.local>
2020-04-08 15:21:07 -0400

    Changes to packaging process

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -11,6 +11,7 @@ set -e
 export GIT_HASH=$(git rev-parse --short HEAD)
 export FILE_DATE=$(date +%Y-%m-%d.%H:%M:%S)
 export FILENAME=$FILE_DATE-$GIT_HASH-$TRAVIS_BRANCH-osx.pkg
+export DEPLOY_VERSION=2.5
 
 cd ./build
 
@@ -21,44 +22,45 @@ if [ -n "${TRAVIS_TAG}" ]; then
   STABLE=true
 fi
 
-#sudo python ../CI/install/osx/build_app.py --public-key ../CI/install/osx/OBSPublicDSAKey.pem --sparkle-framework ../../sparkle/Sparkle.framework --stable=$STABLE
+#sudo python ../CI/install/osx/build_app.py --public-key ../CI/install/osx/EBSPublicDSAKey.pem --sparkle-framework ../../sparkle/Sparkle.framework --stable=$STABLE
 
 ../CI/install/osx/packageApp.sh
 
 # fix obs outputs
-cp /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib ./OBS.app/Contents/Frameworks/
-cp /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib ./OBS.app/Contents/Frameworks/
-cp /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib ./OBS.app/Contents/Frameworks/
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib @executable_path/../Frameworks/libmbedtls.12.dylib ./OBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib @executable_path/../Frameworks/libmbedcrypto.3.dylib ./OBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib @executable_path/../Frameworks/libmbedx509.0.dylib ./OBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/curl/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./OBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change @rpath/libobs.0.dylib @executable_path/../Frameworks/libobs.0.dylib ./OBS.app/Contents/Plugins/obs-outputs.so
+cp /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib ./EBS.app/Contents/Frameworks/
+cp /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib ./EBS.app/Contents/Frameworks/
+cp /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib ./EBS.app/Contents/Frameworks/
+install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib @executable_path/../Frameworks/libmbedtls.12.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib @executable_path/../Frameworks/libmbedcrypto.3.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib @executable_path/../Frameworks/libmbedx509.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+install_name_tool -change /usr/local/opt/curl/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+install_name_tool -change @rpath/libobs.0.dylib @executable_path/../Frameworks/libobs.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
 
 # copy sparkle into the app
 hr "Copying Sparkle.framework"
-cp -r ../../sparkle/Sparkle.framework ./OBS.app/Contents/Frameworks/
-install_name_tool -change @rpath/Sparkle.framework/Versions/A/Sparkle @executable_path/../Frameworks/Sparkle.framework/Versions/A/Sparkle ./OBS.app/Contents/MacOS/obs
+cp -r ../../sparkle/Sparkle.framework ./EBS.app/Contents/Frameworks/
+install_name_tool -change @rpath/Sparkle.framework/Versions/A/Sparkle @executable_path/../Frameworks/Sparkle.framework/Versions/A/Sparkle ./EBS.app/Contents/MacOS/obs
 
 # Copy Chromium embedded framework to app Frameworks directory
-hr "Copying Chromium Embedded Framework.framework"
-sudo mkdir -p OBS.app/Contents/Frameworks
-sudo cp -r ../../cef_binary_${CEF_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded\ Framework.framework OBS.app/Contents/Frameworks/
+# hr "Copying Chromium Embedded Framework.framework"
+# sudo mkdir -p EBS.app/Contents/Frameworks
+# sudo cp -r ../../cef_binary_${CEF_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded\ Framework.framework EBS.app/Contents/Frameworks/
 
-install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./OBS.app/Contents/Plugins/obs-browser.so
-install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./OBS.app/Contents/Plugins/obs-browser.so
-install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./OBS.app/Contents/Plugins/obs-browser.so
+# install_name_tool -change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/obs-browser.so
+# install_name_tool -change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/obs-browser.so
+# install_name_tool -change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/obs-browser.so
 
-cp ../CI/install/osx/OBSPublicDSAKey.pem OBS.app/Contents/Resources
+cp ../CI/install/osx/EBSPublicDSAKey.pem EBS.app/Contents/Resources
 
 # edit plist
-plutil -insert CFBundleVersion -string $GIT_TAG ./OBS.app/Contents/Info.plist
-plutil -insert CFBundleShortVersionString -string $GIT_TAG ./OBS.app/Contents/Info.plist
-plutil -insert OBSFeedsURL -string https://obsproject.com/osx_update/feeds.xml ./OBS.app/Contents/Info.plist
-plutil -insert SUFeedURL -string https://obsproject.com/osx_update/stable/updates.xml ./OBS.app/Contents/Info.plist
-plutil -insert SUPublicDSAKeyFile -string OBSPublicDSAKey.pem ./OBS.app/Contents/Info.plist
+plutil -insert CFBundleVersion -string $DEPLOY_VERSION ./EBS.app/Contents/Info.plist
+plutil -insert CFBundleShortVersionString -string $DEPLOY_VERSION ./EBS.app/Contents/Info.plist
+# plutil -insert EBSFeedsURL -string https://obsproject.com/osx_update/feeds.xml ./EBS.app/Contents/Info.plist
+# plutil -insert SUFeedURL -string https://obsproject.com/osx_update/stable/updates.xml ./EBS.app/Contents/Info.plist
+plutil -insert SUPublicDSAKeyFile -string EBSPublicDSAKey.pem ./EBS.app/Contents/Info.plist
 
-dmgbuild -s ../CI/install/osx/settings.json "OBS" obs.dmg
+#dmgbuild -s ../CI/install/osx/settings.json "EBS" ebs.dmg
+dmgbuild "EBS" ebs.dmg
 
 if [ -v "$TRAVIS" ]; then
 	# Signing stuff
--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,16 +3,16 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 mkdir build
 cd build
 cmake \
--DENABLE_SCRIPTING=OFF \
--DDepsPath=/tmp/obsdeps \
--DCMAKE_BUILD_TYPE=Release \
--DVLCPath=$PWD/../../vlc-master \
--DQTDIR=/usr/local/Cellar/qt/5.14.2 \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
--DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
--Dlibwebrtc_DIR=/tmp/libWebRTC-79.0-x64-Rel-COMMUNITY-BETA/cmake \
--DBUILD_BROWSER=false \
--DOBS_WEBRTC_VENDOR_NAME=Evercast \
--DOBS_VERSION_OVERRIDE=23.2.0 \
+  -DENABLE_SCRIPTING=OFF \
+  -DDepsPath=/tmp/obsdeps \
+  -DCMAKE_BUILD_TYPE=Release \
+  # -DCMAKE_INSTALL_PREFIX=/opt/ebs \
+  -DVLCPath=$PWD/../../vlc-master \
+  -DQTDIR=/usr/local/Cellar/qt/5.14.2 \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
+  -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
+  -Dlibwebrtc_DIR=/tmp/libWebRTC-79.0-x64-Rel-COMMUNITY-BETA/cmake \
+  -DBUILD_BROWSER=false \
+  -DOBS_WEBRTC_VENDOR_NAME=Evercast \
+  -DOBS_VERSION_OVERRIDE=23.2.0 \
 ..
-
