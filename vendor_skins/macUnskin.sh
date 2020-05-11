e816763f482da6810b7788db8094efce27511e82
Dr. Alex <alex.gouaillard@cosmosoftware.io>
2020-05-11 18:55:45 +0200

    WIP: fix the copy in vendor_skin mechanism.

--- vendor_skins/macUnskin.sh
+++ vendor_skins/macUnskin.sh
@@ -1,3 +1,10 @@
+# list all files with .old extention
+# for each file in the list
+#   if the file is the same as its pendent in /vendor_skin/<vendor>
+#     delete
+#   else
+#     copy back in vendor_skin
+#
 git checkout -- CI/*
 rm UI/webrtcVersion.h
 git checkout -- UI/*
