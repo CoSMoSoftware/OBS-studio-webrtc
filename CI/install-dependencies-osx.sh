c051d27a46ff3f969a14bdb704bce4e0fbb2c8ca
Dr Alexandre Gouaillard <alex.gouaillard@cosmosoftware.io>
2020-05-13 18:22:38 +0200

    Use "official" brew qt package

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -11,9 +11,16 @@ cd ../
 
 brew update
 
-#Base OBS Deps and ccache
-brew install jack speexdsp ccache mbedtls
-brew install https://gist.githubusercontent.com/DDRBoxman/b3956fab6073335a4bf151db0dcbd4ad/raw/ed1342a8a86793ea8c10d8b4d712a654da121ace/qt.rb
+# Base OBS Deps and ccache
+brew install jack
+brew install speexdsp
+brew install ccache
+brew install mbedtls
+
+# replace obs one, by brew one.
+# brew install https://gist.githubusercontent.com/DDRBoxman/b3956fab6073335a4bf151db0dcbd4ad/raw/ed1342a8a86793ea8c10d8b4d712a654da121ace/qt.rb
+brew upgrade qt5
+
 brew install https://gist.githubusercontent.com/DDRBoxman/4cada55c51803a2f963fa40ce55c9d3e/raw/572c67e908bfbc1bcb8c476ea77ea3935133f5b5/swig.rb
 
 # Install Packages app so we can build a package later
@@ -21,17 +28,16 @@ brew install https://gist.githubusercontent.com/DDRBoxman/4cada55c51803a2f963fa4
 wget --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg
 sudo installer -pkg ./Packages.pkg -target /
 
-#Base OBS Deps and ccache
-
+# Set up ccache
 export PATH=/usr/local/opt/ccache/libexec:$PATH
 ccache -s || echo "CCache is not available."
 
-# Fetch and untar prebuilt OBS deps that are compatible with older versions of OSX
+# Fetch and untar prebuilt OBS deps that are compatible with older versions of OSX (10.11)
 wget --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz
 tar -xzf ./osx-deps.tar.gz -C /tmp
 
 # if you have your own libwebrtc already installed, comment the following paragraph out.
-# Fetch libwebrtc 73 Community Edition
+# Fetch libwebrtc 79 Community Edition
 wget --retry-connrefused --waitretry=1 https://libwebrtc-community-builds.s3.amazonaws.com/libWebRTC-79-mac.tar.gz
 tar -xf ./libWebRTC-79-mac.tar.gz -C /tmp
 
