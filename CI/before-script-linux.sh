c014580311130f828cf651fb58a5d438b79f8b0d
Stéphane Lepin <stephane.lepin@gmail.com>
2020-11-03 03:03:55 +0100

    ci(azure,linux): don't provide libwebrtc_DIR

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+
 set -ex
-
 ccache -s || echo "CCache is not available."
-
-BASE_PATH="$(pwd)"
 mkdir build && cd build
-cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DWEBRTC_INCLUDE_DIR="${BASE_PATH}/libwebrtc/include" -DWEBRTC_LIB="${BASE_PATH}/libwebrtc/lib/libwebrtc.a" -Dlibwebrtc_DIR="${BASE_PATH}/libwebrtc/cmake" ..
+cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DWEBRTC_INCLUDE_DIR="../libwebrtc/include" -DWEBRTC_LIB="../libwebrtc/lib/libwebrtc.a" ..
