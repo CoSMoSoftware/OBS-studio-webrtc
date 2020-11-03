700347456bde5e0d2ff624a7a2d99c63a04e610d
Stéphane Lepin <stephane.lepin@gmail.com>
2020-11-03 02:56:47 +0100

    ci(linux): use targeted include folder and lib on that platform + use absolute paths for libwebrtc

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 set -ex
+
 ccache -s || echo "CCache is not available."
+
+BASE_PATH="$(pwd)"
 mkdir build && cd build
-cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DWEBRTC_ROOT_DIR="../libwebrtc" -Dlibwebrtc_DIR="../libwebrtc/cmake" ..
+cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DWEBRTC_INCLUDE_DIR="${BASE_PATH}/libwebrtc/include" -DWEBRTC_LIB="${BASE_PATH}/libwebrtc/lib/libwebrtc.a" -Dlibwebrtc_DIR="${BASE_PATH}/libwebrtc/cmake" ..
