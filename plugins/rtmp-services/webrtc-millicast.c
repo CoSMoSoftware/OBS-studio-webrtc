b510d8cda52ee14fa263ba692883f40dcb09bd3e
ludo <ludovic.roux@cosmosoftware.io>
2020-11-17 10:44:43 +0800

    Millicast: do not ask for a room name

--- plugins/rtmp-services/webrtc-millicast.c
+++ plugins/rtmp-services/webrtc-millicast.c
@@ -109,6 +109,9 @@ static obs_properties_t *webrtc_millicast_properties(void *unused)
   obs_property_list_add_string(obs_properties_get(ppts, "codec"),"vp8", "vp8");
   obs_property_list_add_string(obs_properties_get(ppts, "codec"),"vp9", "vp9");
 
+  p = obs_properties_get(ppts, "room");
+  obs_property_set_visible(p, false);
+
 	p = obs_properties_get(ppts, "username");
 	obs_property_set_visible(p, true);
 
