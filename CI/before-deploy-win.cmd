448116836c897a162eeee9b88f585aadac7edcaa
Stéphane Lepin <stephane.lepin@gmail.com>
2020-11-18 23:28:57 +0100

    ci(azure,win): fix packaging

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,3 +1,3 @@
-robocopy .\build32\rundir\RelWithDebInfo .\build\ /E /XF .gitignore
-robocopy .\build64\rundir\RelWithDebInfo .\build\ /E /XC /XN /XO /XF .gitignore
+robocopy .\build32\rundir\Release .\build\ /E /XF .gitignore
+robocopy .\build64\rundir\Release .\build\ /E /XC /XN /XO /XF .gitignore
 7z a build.zip .\build\*
