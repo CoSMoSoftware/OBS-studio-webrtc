357223145101a21f50174ca35e309a2fb929a762
Stéphane Lepin <stephane.lepin@gmail.com>
2020-11-25 03:12:40 +0100

    ci(linux): add package step

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -3,4 +3,4 @@
 set -ex
 ccache -s || echo "CCache is not available."
 mkdir build && cd build
-cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DLibWebRTC_DIR="../libwebrtc/cmake" -DWEBRTC_INCLUDE_DIR="../libwebrtc/include" -DWEBRTC_LIB="../libwebrtc/lib/libwebrtc.a" ..
+cmake -DUNIX_STRUCTURE=1 -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DLibWebRTC_DIR="../libwebrtc/cmake" -DWEBRTC_INCLUDE_DIR="../libwebrtc/include" -DWEBRTC_LIB="../libwebrtc/lib/libwebrtc.a" ..
