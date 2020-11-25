907847e58b2205e90dec0cbac05c6f4d60037c26
Stéphane Lepin <stephane.lepin@gmail.com>
2020-11-25 02:30:00 +0100

    ci(linux): debug cont.

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -3,6 +3,7 @@
 set -ex
 ccache -s || echo "CCache is not available."
 mkdir build && cd build
+ls ..
 ls ../libwebrtc/include/
 ls ../libwebrtc/include/api
 cmake -DBUILD_CAPTIONS=ON -DBUILD_BROWSER=ON -DCEF_ROOT_DIR="../cef_binary_${CEF_BUILD_VERSION}_linux64" -DLibWebRTC_DIR="../libwebrtc/cmake" -DWEBRTC_INCLUDE_DIR="../libwebrtc/include" -DWEBRTC_LIB="../libwebrtc/lib/libwebrtc.a" ..
