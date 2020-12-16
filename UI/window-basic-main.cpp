2b15030a28c5bf0f2eb26669cb32e6a838589f82
Dr alex gouaillard <alex.gouaillard@cosmosoftware.io>
2020-12-16 09:59:09 +0800

    BUG: fixes hotkey crashes tickets #238 and #250.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -2199,6 +2199,11 @@ void OBSBasic::InitHotkeys()
 
 void OBSBasic::ProcessHotkey(obs_hotkey_id id, bool pressed)
 {
+	// Intercept keyboard event when OBS is not the window being focussed
+	if( QApplication::activeWindow() == nullptr )
+ 		return;
+
+        // we're focussed, dispatch the keyboard/hotkey event
 	obs_hotkey_trigger_routed_callback(id, pressed);
 }
 
