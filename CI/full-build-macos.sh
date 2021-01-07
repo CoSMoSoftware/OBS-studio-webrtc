16583e608bbc6504535996f416b8e8fb9941d2bc
ludo <ludovic.roux@cosmosoftware.io>
2021-01-07 10:24:35 +0800

    mac: fix plugins ndi/websocket path to Qt

--- CI/full-build-macos.sh
+++ CI/full-build-macos.sh
@@ -340,7 +340,9 @@ bundle_dylibs() {
         -x ./OBS-WebRTC.app/Contents/PlugIns/obs-x264.so \
         -x ./OBS-WebRTC.app/Contents/PlugIns/text-freetype2.so \
         -x ./OBS-WebRTC.app/Contents/PlugIns/obs-libfdk.so \
-        -x ./OBS-WebRTC.app/Contents/PlugIns/obs-outputs.so
+        -x ./OBS-WebRTC.app/Contents/PlugIns/obs-outputs.so \
+        -x ./OBS-WebRTC.app/Contents/PlugIns/obs-ndi.so \
+        -x ./OBS-WebRTC.app/Contents/PlugIns/obs-websocket.so
 #        -x ./OBS-WebRTC.app/Contents/PlugIns/linux-jack.so \
     step "Move libobs-opengl to final destination"
     cp ./libobs-opengl/libobs-opengl.so ./OBS-WebRTC.app/Contents/Frameworks
