0ed7141fa7a3dcc9adf1f25200f3eac54a1ea14b
ludo <ludovic.roux@cosmosoftware.io>
2021-01-14 19:32:52 +0800

    replaced libwebrtc M86 community by M86 with H264

--- CI/install-dependencies-win.cmd
+++ CI/install-dependencies-win.cmd
@@ -1,10 +1,10 @@
 if exist dependencies2017.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2017.zip -f --retry 5 -z dependencies2017.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2017.zip -f --retry 5 -C -)
 if exist vlc.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 -z vlc.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 -C -)
 if exist cef_binary_%CEF_VERSION%_windows64_minimal.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows64_minimal.zip -f --retry 5 -z cef_binary_%CEF_VERSION%_windows64_minimal.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows64_minimal.zip -f --retry 5 -C -)
-if exist libWebRTC-84.0-x64-Release-H264.exe (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH% -f --retry 5 -z libWebRTC-%LIBWEBRTC_VERSION%-x64-RelComm.exe) else (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH% -f --retry 5 -C -)
+if exist libWebRTC-84.0-x64-Release-H264.exe (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH% -f --retry 5 -z libWebRTC-84.0-x64-Release-H264.exe) else (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH% -f --retry 5 -C -)
 if exist openssl-1.1.tgz (curl -kLO https://libwebrtc-community-builds.s3.amazonaws.com/openssl-1.1.tgz -f --retry 5 -z openssl-1.1.tgz) else                 (curl -kLO https://libwebrtc-community-builds.s3.amazonaws.com/openssl-1.1.tgz -f --retry 5 -C -)
 7z x dependencies2017.zip -odependencies2017
 7z x vlc.zip -ovlc
 7z x cef_binary_%CEF_VERSION%_windows64_minimal.zip -oCEF_64
-"libWebRTC-%LIBWEBRTC_VERSION%-x64-RelComm.exe" /S /SD /D="%CD%\libwebrtc"
+"libWebRTC-84.0-x64-Release-H264.exe" /S /SD /D="%CD%\libwebrtc"
 tar -xzf openssl-1.1.tgz
