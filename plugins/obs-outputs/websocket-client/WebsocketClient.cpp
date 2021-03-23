710a246c06de498ab963509793ae590290bf587c
Dr alex gouaillard <alex.gouaillard@cosmosoftware.io>
2021-03-23 09:52:30 +0700

    WIP: websocket client is not an obs module anymore.

--- plugins/obs-outputs/websocket-client/WebsocketClient.cpp
+++ plugins/obs-outputs/websocket-client/WebsocketClient.cpp
@@ -1,17 +1,8 @@
 /* Copyright Dr. Alex. Gouaillard (2015, 2020) */
 
-#include <obs-module.h>
 #include <openssl/opensslv.h>
 #include "MillicastWebsocketClientImpl.h"
 #include "CustomWebrtcImpl.h"
-
-OBS_DECLARE_MODULE()
-
-bool obs_module_load(void)
-{
-	OPENSSL_init_ssl(0, NULL);
-	return true;
-}
 
 WEBSOCKETCLIENT_API WebsocketClient *createWebsocketClient(int type)
 {
