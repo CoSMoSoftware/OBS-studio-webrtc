f207c7643136404bf8adafc912d4dd6993dcd2dd
Ludovic Roux <ludovic.roux@cosmosoftware.io>
2021-06-10 18:05:21 +0800

    compile without browser

--- CI/install-script-win.cmd
+++ CI/install-script-win.cmd
@@ -6,7 +6,7 @@ if exist cef_binary_%CEF_VERSION%_windows64_minimal.zip (curl -kLO https://cdn-f
 7z x cef_binary_%CEF_VERSION%_windows64_minimal.zip -oCEF_64
 set DepsPath64=%CD%\dependencies2019\win64
 set VLCPath=%CD%\vlc
-set QTDIR64=C:\QtDep\5.15.2\msvc2019_64
+set QTDIR64=C:\QtDep\Qt\5.15.2\msvc2019_64
 set CEF_64=%CD%\CEF_64\cef_binary_%CEF_VERSION%_windows64_minimal
 set build_config=RelWithDebInfo
 set VIRTUALCAM-GUID=A3FCE0F5-3493-419F-958A-ABA1250EC20B
@@ -19,7 +19,7 @@ if "%1"=="Millicast" (
 )
 mkdir build64_%1
 cd build64_%1
-cmake %vendor_option% -G "Visual Studio 16 2019" -A x64 -DBUILD_NDI=ON -DBUILD_WEBSOCKET=ON -DLibObs_DIR=%CD%\libobs -DLIBOBS_LIB=%CD%\libobs\Release\obs.lib -Dw32-pthreads_DIR=%CD%\deps\w32-pthreads -DOBS_FRONTEND_LIB=%CD%\UI\obs-frontend-api\Release\obs-frontend-api.lib -DLIBOBS_INCLUDE_DIR=%CD%\..\libobs -DQt5_DIR=%QTDIR64%\lib\cmake\Qt5 -DCMAKE_SYSTEM_VERSION=10.0 -DOBS_VERSION_OVERRIDE=%OBS_VERSION% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DENABLE_VLC=ON -DCOMPILE_D3D12_HOOK=true -DBUILD_BROWSER=true -DCEF_ROOT_DIR=%CEF_64% -Dlibwebrtc_DIR="%CD%\libwebrtc\cmake" -DOPENSSL_ROOT_DIR="%CD%\..\openssl-1.1\x64" -DVIRTUALCAM_GUID="%VIRTUALCAM-GUID%" -DQt5Widgets_DIR=%QTDIR64%\lib\cmake\Qt5Widgets -DQt5Svg_DIR=%QTDIR64%\lib\cmake\Qt5Svg -DQt5Xml_DIR=%QTDIR64%\lib\cmake\Qt5Xml ..
+cmake %vendor_option% -G "Visual Studio 16 2019" -A x64 -DBUILD_NDI=ON -DBUILD_WEBSOCKET=ON -DLibObs_DIR=%CD%\libobs -DLIBOBS_LIB=%CD%\libobs\Release\obs.lib -Dw32-pthreads_DIR=%CD%\deps\w32-pthreads -DOBS_FRONTEND_LIB=%CD%\UI\obs-frontend-api\Release\obs-frontend-api.lib -DLIBOBS_INCLUDE_DIR=%CD%\..\libobs -DQt5_DIR=%QTDIR64%\lib\cmake\Qt5 -DCMAKE_SYSTEM_VERSION=10.0 -DOBS_VERSION_OVERRIDE=%OBS_VERSION% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DENABLE_VLC=ON -DCOMPILE_D3D12_HOOK=true -DBUILD_BROWSER=false -DCEF_ROOT_DIR=%CEF_64% -Dlibwebrtc_DIR="%CD%\libwebrtc\cmake" -DOPENSSL_ROOT_DIR="%CD%\..\openssl-1.1\x64" -DVIRTUALCAM_GUID="%VIRTUALCAM-GUID%" -DQt5Widgets_DIR=%QTDIR64%\lib\cmake\Qt5Widgets -DQt5Svg_DIR=%QTDIR64%\lib\cmake\Qt5Svg -DQt5Xml_DIR=%QTDIR64%\lib\cmake\Qt5Xml ..
 if "%1"=="Millicast" (
   move obs-studio.sln Millicast.sln
 )
