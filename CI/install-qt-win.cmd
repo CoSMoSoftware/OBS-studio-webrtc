be3678c516c567fcbbfd4aece0cd7409bd601213
ludo <ludovic.roux@cosmosoftware.io>
2021-10-17 12:12:16 +0800

    windows: install full Qt 5.15.2

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,5 +1,4 @@
-if exist Qt_5.15.2.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.15.2.7z -f --retry 5 -z Qt_5.15.2.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.15.2.7z -f --retry 5 -C -)
-# if exist Qt_5.15.2.7z (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH_PREFIX%/Qt_5.15.2.7z -f --retry 5 -z Qt_5.15.2.7z) else (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH_PREFIX%/Qt_5.15.2.7z -f --retry 5 -C -)
+if exist Qt_5.15.2.7z (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH_PREFIX%/Qt_5.15.2.7z -f --retry 5 -z Qt_5.15.2.7z) else (curl -u %FTP_LOGIN%:%FTP_PASSWORD% -kLO %FTP_PATH_PREFIX%/Qt_5.15.2.7z -f --retry 5 -C -)
 7z x Qt_5.15.2.7z -oQt
 mv Qt C:\QtDep
 dir C:\QtDep
