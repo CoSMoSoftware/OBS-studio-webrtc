512e29b437b948c188dc6206db7abfdd7f1f4c56
ludo <ludovic.roux@cosmosoftware.io>
2021-10-29 16:26:01 +0800

    win: change of chocolatey version

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,9 +1,11 @@
 REM install WiX Toolset
 choco -y install wixtoolset
 
+echo $Env:Path
+
 REM Parameter %1 = vendor name
 cd build64_%1
-'C:\Program Files\CMake\bin\cpack.exe' -G WIX
+'$Env:Systemroot\Program Files\CMake\bin\cpack.exe' -G WIX
 
 mkdir ..\build_%1
 dir
