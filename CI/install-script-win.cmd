3d92072702bb5a664641edeccd1dab4f41a54e29
ludo <ludovic.roux@cosmosoftware.io>
2022-03-09 10:04:05 +0800

    windows CEF

--- CI/install-script-win.cmd
+++ CI/install-script-win.cmd
@@ -1,13 +1,14 @@
-if exist dependencies2019.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 -z dependencies2019.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 -C -)
-if exist vlc.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 -z vlc.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 -C -)
-if exist cef_binary_%CEF_VERSION%_windows64_minimal.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows64_minimal.zip -f --retry 5 -z cef_binary_%CEF_VERSION%_windows64_minimal.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows64_minimal.zip -f --retry 5 -C -)
+if exist dependencies2019.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 dependencies2019.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 -C -)
+if exist vlc.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 vlc.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/vlc.zip -f --retry 5 -C -)
+         cef_binary_${{ env.WINDOWS_CEF_BUILD_VERSION }}_windows_x64
+if exist cef_binary_%CEF_VERSION%_windows_x64.zip (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows_x64.zip -f --retry 5 cef_binary_%CEF_VERSION%_windows_x64.zip) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/cef_binary_%CEF_VERSION%_windows_x64.zip -f --retry 5 -C -)
 7z x dependencies2019.zip -odependencies2019
 7z x vlc.zip -ovlc
-7z x cef_binary_%CEF_VERSION%_windows64_minimal.zip -oCEF_64
+7z x cef_binary_%CEF_VERSION%_windows_x64.zip -oCEF_64
 set DepsPath64=%CD%\dependencies2019\win64
 set VLCPath=%CD%\vlc
 set QTDIR64=C:\QtDep\Qt\5.15.2\msvc2019_64
-set CEF_64=%CD%\CEF_64\cef_binary_%CEF_VERSION%_windows64_minimal
+set CEF_64=%CD%\CEF_64\cef_binary_%CEF_VERSION%_windows_x64
 set build_config=RelWithDebInfo
 set VIRTUALCAM-GUID=A3FCE0F5-3493-419F-958A-ABA1250EC20B
 git stash
