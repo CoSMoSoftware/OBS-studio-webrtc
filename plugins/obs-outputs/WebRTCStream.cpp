29527d87df10e34399b6f87afc968da9431459a5
ludo <ludovic.roux@cosmosoftware.io>
2022-08-01 06:53:43 +0200

    fix crash when change of video color format (NV12/I420/I444)

--- plugins/obs-outputs/WebRTCStream.cpp
+++ plugins/obs-outputs/WebRTCStream.cpp
@@ -710,10 +710,6 @@ void WebRTCStream::onRemoteIceCandidate(const std::string &sdpData)
 
 void WebRTCStream::onOpened(const std::string &sdp)
 {
-	// Wait for 'output' structure to be updated by OBS
-	// in case of a change in video color format (NV12/I420/I444)
-	std::this_thread::sleep_for (std::chrono::seconds(1));
-
 	info("ANSWER:\n\n%s\n", sdp.c_str());
 
 	std::string sdpCopy = sdp;
@@ -746,7 +742,12 @@ void WebRTCStream::onOpened(const std::string &sdp)
 	obs_output_set_audio_conversion(output, &conversion);
 
 	info("Begin data capture...");
-	obs_output_begin_data_capture(output, 0);
+	try {
+		obs_output_begin_data_capture(output, 0);
+	} catch (...) {
+		info("ERROR: Start streaming failed");
+		return;
+	}
 }
 
 void WebRTCStream::OnSetRemoteDescriptionComplete(webrtc::RTCError error)
