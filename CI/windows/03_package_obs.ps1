90ee213d8e7df11627d5cdae7807b72b435d6ece
ludocosmo <ludovic.roux@cosmosoftware.io>
2022-10-02 17:28:09 +0800

    CI windows: package

--- CI/windows/03_package_obs.ps1
+++ CI/windows/03_package_obs.ps1
@@ -8,7 +8,7 @@ Param(
     [ValidateSet('x86', 'x64')]
     [String]$BuildArch = $(if (Test-Path variable:BuildArch) { "${BuildArch}" } else { ('x86', 'x64')[[System.Environment]::Is64BitOperatingSystem] }),
     [ValidateSet("Release", "RelWithDebInfo", "MinSizeRel", "Debug")]
-    [String]$BuildConfiguration = $(if (Test-Path variable:BuildConfiguration) { "${BuildConfiguration}" } else { "RelWithDebInfo" })
+    [String]$BuildConfiguration = $(if (Test-Path variable:BuildConfiguration) { "${BuildConfiguration}" } else { "RelWithDebInfo" }),
     [ValidateSet("Millicast", "Wowza", "RemoteFilming", "RemoteFilming-A", "RemoteFilming-B", "RemoteFilming-C", "RemoteFilming-D")]
     [String]$Vendor = $(if (Test-Path variable:Vendor) { "${Vendor}" } else { "Millicast" })
 )
