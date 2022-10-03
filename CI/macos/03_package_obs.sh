b3c86eacec093035de4380311885cc8012ef60cf
ludocosmo <ludovic.roux@cosmosoftware.io>
2022-10-03 09:58:36 +0800

    CI mac: appspecific password

--- CI/macos/03_package_obs.sh
+++ CI/macos/03_package_obs.sh
@@ -77,7 +77,9 @@ notarize_obs() {
 
         # CODESIGN_IDENT_SHORT=$(echo "${CODESIGN_IDENT}" | /usr/bin/sed -En "s/.+\((.+)\)/\1/p")
         echo -n "${COLOR_ORANGE}"
+        step "store app specific password..."
         /usr/bin/xcrun notarytool store-credentials "OBS-Codesign-Password" --apple-id "${CODESIGN_IDENT_USER}" --team-id "${CODESIGN_TEAM_ID}" --password "${CODESIGN_IDENT_PASS}"
+        step "store app specific password... complleted"
         echo -n "${COLOR_RESET}"
 
         step "Notarize ${NOTARIZE_TARGET}..."
