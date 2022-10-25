403889161de6e1f5e88530e61cd4b1a5ccfafa85
ludo <ludovic.roux@cosmosoftware.io>
2022-10-25 10:58:58 +0800

    no obs-ndi plugin

--- CI/linux/02_build_obs.sh
+++ CI/linux/02_build_obs.sh
@@ -77,7 +77,7 @@ _configure_obs() {
         ${RESTREAM_OPTIONS} \
         ${CI:+-DENABLE_UNIT_TESTS=ON -DBUILD_FOR_DISTRIBUTION=${BUILD_FOR_DISTRIBUTION} -DOBS_BUILD_NUMBER=${GITHUB_RUN_ID}} \
         ${QUIET:+-Wno-deprecated -Wno-dev --log-level=ERROR} \
-        -DBUILD_NDI=ON \
+        -DBUILD_NDI=OFF \
         -DLIBOBS_INCLUDE_DIR=${CMAKE_SOURCE_DIR}/libobs \
         -DLIBOBS_LIB=${BUILD_DIR}/libobs/libobs.so \
         -DUNIX_STRUCTURE=1 \
